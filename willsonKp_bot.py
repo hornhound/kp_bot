8bdfec3a2614ff437ee94d4b80f166f6c335516f
BH <runescape_rawks@hotmail.com>
2017-10-12 20:40:24 +0800

    added handlers

--- willsonKp_bot.py
+++ willsonKp_bot.py
@@ -23,3 +23,7 @@ def kp(bot,update):
 	if update.message.from_user.username == "Willsoncy":
 		idx = random.randint(0,1)
 		bot.sendMessage(chat_id = update.message.chat_id, text = list_of_insults[idx],reply_to_message_id=update.message.message_id)
+
+
+dispatcher.add_handler(CommandHandler('start', start))
+kp_handler = MessageHandler(Filters.text,kp)
