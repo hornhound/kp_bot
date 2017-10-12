6baeb5e1a51187302d86c85864f6d0cbcb8be5e0
hornhound <32737822+hornhound@users.noreply.github.com>
2017-10-12 20:28:01 +0800

    Added webhook implementation

--- willsonKp_bot.py
+++ willsonKp_bot.py
@@ -3,10 +3,15 @@ from telegram.ext import Updater, CommandHandler
 import psycopg2
 import urlparse
 import random
+#Initialization for webhook
+TOKEN = '373531294:AAG5aWFzIcBLxA-qRg1rK5EkeIklytaIWHs'
+PORT = int(os.environ.get('PORT', '5000'))
 
-bot = telegram.Bot(token = "459235062:AAHdk58qLQc0MiTkgPgh5c6wPKCXUpn7nNg")
-updater = Updater(token = "459235062:AAHdk58qLQc0MiTkgPgh5c6wPKCXUpn7nNg")
+
+bot = telegram.Bot(token = TOKEN)
+updater = Updater(token = TOKEN)
 dispatcher = updater.dispatcher
+bot.setWebhook(webhook_url = "https://willson-kp-bot.herokuapp.com/" + TOKEN)
 
 def start(bot,update):
 	bot.sendMessage(chat_id = update.message.chat_id,
@@ -18,4 +23,3 @@ def kp(bot,update):
 	if update.message.from_user.username == "Willsoncy":
 		idx = random.randint(0,1)
 		bot.sendMessage(chat_id = update.message.chat_id, text = list_of_insults[idx],reply_to_message_id=update.message.message_id)
-
